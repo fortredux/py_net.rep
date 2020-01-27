21ae40bed42be00ecd5e30fc97f599243920b838
fortredux <fortunaredux@protonmail.com>
2020-01-27 23:27:59 +0000

    Done first part of task_18_1

---
+++ exercises/18_db/task_18_1/create_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+import os
+
+
+db_filename = 'dhcp_snooping.db'
+schema_filename = 'dhcp_snooping_schema.sql'
+db_exists = os.path.exists(db_filename)
+
+
+if not db_exists:
+    print('Creating database...')
+
+    conn = sqlite3.connect('dhcp_snooping.db')
+
+    with open(schema_filename) as src:
+        schema = src.read()
+        conn.executescript(schema)
+
+    conn.close()
+
+else:
+    print('Database exists')
+
+print('Done')
