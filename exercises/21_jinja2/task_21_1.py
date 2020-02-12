47e62f8960b24c6d839f70856db9b19c09e5c80e
fortredux <fortunaredux@protonmail.com>
2020-02-12 15:22:27 +0000

    Made task_21_1

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -13,3 +13,41 @@
 Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
 
 '''
+
+from jinja2 import Environment, FileSystemLoader
+import yaml
+
+
+# Variant with yaml.load inside function
+'''
+def generate_config(template, data_dict):
+    template_dir, template_file = template.split('/')
+    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template(template_file)
+
+    with open(data_dict) as f:
+        router = yaml.load(f, Loader=yaml.FullLoader)
+
+    to_return = template.render(router)
+    return to_return
+
+
+if __name__ == "__main__":
+    print(generate_config('templates/for.txt', 'data_files/for.yml'))
+'''
+
+
+# Variant with yaml.load outside function
+def generate_config(template, data_dict):
+    template_dir, template_file = template.split('/')
+    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template(template_file)
+
+    to_return = template.render(data_dict)
+    return to_return
+
+
+if __name__ == "__main__":
+    with open('data_files/for.yml') as f:
+        dic = yaml.load(f, Loader=yaml.FullLoader)
+    print(generate_config('templates/for.txt', dic))
