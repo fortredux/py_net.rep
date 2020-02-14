ad70b29b14242efeb5b3d85aa18119f4efc3ae9a
fortredux <fortunaredux@protonmail.com>
2020-02-14 17:52:17 +0000

    Done task_20_2

--- exercises/20_concurrent_connections/task_20_2.py
+++ exercises/20_concurrent_connections/task_20_2.py
@@ -33,3 +33,62 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml
 '''
+
+
+from itertools import repeat
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import yaml
+from netmiko import ConnectHandler
+
+
+def send_command(device_params, command):
+    with ConnectHandler(**device_params) as ssh:
+                ssh.enable()
+                first_line = ssh.find_prompt() + command + '\n'
+                result = ssh.send_command(command)
+    return first_line + result
+
+
+# Variant with concurrent.futures and submit
+'''
+def send_show_command_to_devices(devices, command, filename, limit=3):
+    to_return = ''
+
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        future_list = []
+
+        for device in devices:
+            future = executor.submit(send_command, device, command)
+            future_list.append(future)
+
+        for f in as_completed(future_list):
+            result = f.result()
+            to_return += result + '\n'
+
+    with open(filename, 'w') as dest:
+        dest.write(to_return)
+'''
+
+
+# Variant with concurrent.futures and map
+
+def send_show_command_to_devices(devices, command, filename, limit=3):
+    to_return = ''
+
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        result = executor.map(send_command, devices, repeat(command))
+        for r in result:
+            to_return += r + '\n'
+
+    with open(filename, 'w') as dest:
+        dest.write(to_return)
+
+
+if __name__ == '__main__':
+    with open('devices.yaml') as f:
+        dictionaries = yaml.load(f, Loader=yaml.FullLoader)
+
+    send_show_command_to_devices(dictionaries, 'sh ip int br', 'output.txt')
+
+    print(open('output.txt').read())
