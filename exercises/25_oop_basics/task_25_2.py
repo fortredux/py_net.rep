54371e54b4a2d3e460d1280dda275439040e151f
fortredux <fortunaredux@protonmail.com>
2020-03-01 20:59:59 +0000

    Made task_25_2

--- exercises/25_oop_basics/task_25_2.py
+++ exercises/25_oop_basics/task_25_2.py
@@ -22,10 +22,55 @@ In [3]: r1_params = {
    ...:     'password': 'cisco',
    ...:     'secret': 'cisco'}
    ...:
+Out[5]: 'sh ip int br\r\nInterface                  IP-Address      OK? Method Status                Protocol\r\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \r\nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \r\nEthernet0
 
 In [4]: r1 = CiscoTelnet(**r1_params)
 
-In [5]: r1.send_show_command('sh ip int br')
-Out[5]: 'sh ip int br\r\nInterface                  IP-Address      OK? Method Status                Protocol\r\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \r\nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \r\nEthernet0/2                190.16.200.1    YES NVRAM  up                    up      \r\nEthernet0/3                192.168.130.1   YES NVRAM  up                    up      \r\nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \r\nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \r\nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      \r\nLoopback0                  10.1.1.1        YES NVRAM  up                    up      \r\nLoopback55                 5.5.5.5         YES manual up                    up      \r\nR1#'
+In [5]: r1.send_show_command('sh ip int br')/2                190.16.200.1    YES NVRAM  up                    up      \r\nEthernet0/3                192.168.130.1   YES NVRAM  up                    up      \r\nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \r\nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \r\nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      \r\nLoopback0                  10.1.1.1        YES NVRAM  up                    up      \r\nLoopback55                 5.5.5.5         YES manual up                    up      \r\nR1#'
 
 '''
+
+
+import telnetlib
+import time
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.secret = secret
+
+        self.connection = telnetlib.Telnet(self.ip)
+        self.connection.read_until(b'Username')
+        self._write_line(self.username)
+        self.connection.read_until(b'Password')
+        self._write_line(self.password)
+        self._write_line('enable')
+        self.connection.read_until(b'Password:')
+        self._write_line(self.secret)
+        self._write_line('terminal length 0')
+        self.connection.read_until(b'#')
+        print(f'Connection to device {ip} established')
+
+    def _write_line(self, line):
+        line = bytes(line, 'utf-8')
+        return self.connection.write(line + b"\r\n")
+
+    def send_show_command(self, command):
+        send = self._write_line(command)
+        time.sleep(1)
+        self.connection.read_until(b'#')
+        output = self.connection.read_very_eager().decode('ascii')
+        return output
+
+if __name__ == "__main__":
+    r1_params = {
+            'ip': '192.168.100.1',
+            'username': 'cisco',
+            'password': 'cisco',
+            'secret': 'cisco',
+            }
+    r1 = CiscoTelnet(**r1_params)
+    print(r1.send_show_command('sh ip int br'))
