0f90cfcaa3c0c0cfd1bc6b4760d15f7f1fd58fab
fortredux <fortunaredux@protonmail.com>
2020-03-11 21:24:05 +0000

    Done tasks 22_1 and 22_1a

--- exercises/22_textfsm/task_22_1.py
+++ exercises/22_textfsm/task_22_1.py
@@ -14,3 +14,39 @@
 
 '''
 
+
+import textfsm
+
+"""
+def parse_command_output(template, command_output):
+    final_list = [["Interface", "IP-Address", "Status", "Protocol"]]
+
+    with open(template) as temp, open(command_output) as output:
+        fsm = textfsm.TextFSM(temp)
+        result = fsm.ParseText(output.read())
+        final_list.append(result)
+    return final_list
+
+
+if __name__ == "__main__":
+    from tabulate import tabulate
+    table_data = parse_command_output('templates/sh_ip_int_br.template', 'output/sh_ip_int_br.txt')
+    print(tabulate(table_data[1], headers=table_data[0]))
+"""
+
+
+def parse_command_output(template, command_output):
+
+    with open(template) as temp:
+        fsm = textfsm.TextFSM(temp)
+        result = fsm.ParseText(command_output)
+        result.insert(0, fsm.header)
+    return result
+
+
+if __name__ == "__main__":
+    from tabulate import tabulate
+    with open('output/sh_ip_int_br.txt') as f:
+        output = f.read()
+    table_data = parse_command_output('templates/sh_ip_int_br.template', output)
+    print(tabulate(table_data, headers='firstrow'))
--- exercises/22_textfsm/task_22_1a.py
+++ exercises/22_textfsm/task_22_1a.py
@@ -14,3 +14,24 @@
 
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt и шаблоне templates/sh_ip_int_br.template.
 '''
+
+
+import textfsm
+
+
+def parse_output_to_dict(template, command_output):
+
+    with open(template) as temp:
+        fsm = textfsm.TextFSM(temp)
+        result = fsm.ParseText(command_output)
+    to_return = []
+    for r in result:
+        to_return.append(dict(zip(fsm.header, r)))
+    return to_return
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+    with open('output/sh_ip_int_br.txt') as f:
+        output = f.read()
+    pprint(parse_output_to_dict('templates/sh_ip_int_br.template', output))
