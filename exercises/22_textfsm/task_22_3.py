8b5e1a03374a83b270be36e9826fde283300d6ec
fortredux <fortunaredux@protonmail.com>
2020-03-13 23:04:22 +0000

    Done tasks 22_3 and 22_4

--- exercises/22_textfsm/task_22_3.py
+++ exercises/22_textfsm/task_22_3.py
@@ -18,3 +18,40 @@
 
 Проверить работу функции на примере вывода команды sh ip int br.
 '''
+
+
+import textfsm
+import clitable
+
+
+def parse_command_dynamic(comand_output, attributes_dict, index_file="index", templ_path="templates"):
+    cli_table = clitable.CliTable(index_file, templ_path)
+    cli_table.ParseCmd(comand_output, attributes_dict)
+
+    #to_return = [dict(zip(cli_table.header, row)) for row in cli_table]
+
+    to_return = []
+
+    for row in cli_table:
+        to_return.append(dict(zip(cli_table.header, row)))
+
+    '''
+    headers = list(cli_table.header)
+    for row in cli_table:
+        temp = {}
+        for i in range(len(list(row))):
+            temp[headers[i]] = row[i]
+        to_return.append(temp)
+    '''
+
+    return to_return
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    with open("output/sh_ip_int_br.txt") as f:
+        f = f.read()
+
+    attributes = {'Command': 'show ip int br', 'Vendor': 'cisco_ios'}
+    pprint(parse_command_dynamic(f, attributes))
--- exercises/22_textfsm/task_22_4.py
+++ exercises/22_textfsm/task_22_4.py
@@ -18,3 +18,33 @@
 
 Проверить работу функции на примере вывода команды sh ip int br и устройствах из devices.yaml.
 '''
+
+
+import textfsm
+import clitable
+import yaml
+from netmiko import ConnectHandler
+
+
+def send_and_parse_show_command(device_dict, command, templates_path):
+
+    with ConnectHandler(**device_dict) as ssh:
+        ssh.enable()
+        result = ssh.send_command(command)
+
+    attribute ={}
+    attribute['Command'] = command
+
+    cli_table = clitable.CliTable('index', templates_path)
+    cli_table.ParseCmd(result, attribute)
+
+    to_return = [dict(zip(cli_table.header, row)) for row in cli_table]
+
+    return to_return
+
+if __name__ == "__main__":
+    with open('devices.yaml') as d:
+        devices = yaml.load(d, Loader=yaml.FullLoader)
+        first_device = devices[0]
+
+    print(send_and_parse_show_command(first_device, 'sh ip int br', 'templates'))
