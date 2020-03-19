b49d280d44c68938577e2679fdefac600ecc1c4e
fortredux <fortunaredux@protonmail.com>
2020-03-19 22:01:48 +0000

    Done task_26_2

--- exercises/26_oop_special_methods/task_26_2.py
+++ exercises/26_oop_special_methods/task_26_2.py
@@ -42,3 +42,55 @@ ValueError                                Traceback (most recent call last)
 ValueError: Возникла ошибка
 '''
 
+
+import telnetlib
+import time
+import yaml
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.secret = secret
+
+        self.connection = telnetlib.Telnet(self.ip)
+        self.connection.read_until(b'Username')
+        self._write_line(self.username)
+        self.connection.read_until(b'Password')
+        self._write_line(self.password)
+        self._write_line('enable')
+        self.connection.read_until(b'Password:')
+        self._write_line(self.secret)
+        self._write_line('terminal length 0')
+        self.connection.read_until(b'#')
+        print(f'Connection to device {ip} established')
+
+    def _write_line(self, line):
+        line = bytes(line, 'utf-8')
+        return self.connection.write(line + b"\r\n")
+
+    def send_show_command(self, command):
+        send = self._write_line(command)
+        time.sleep(1)
+        self.connection.read_until(b'#')
+        output = self.connection.read_very_eager().decode('ascii')
+        return output
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type,exc_value, traceback):
+        self.connection.close()
+
+if __name__ == "__main__":
+    with open('devices.yaml') as d:
+        devices = yaml.load(d, Loader=yaml.FullLoader)
+        first_device = devices[0]
+
+    #r1 = CiscoTelnet(**first_device)
+    with CiscoTelnet(**first_device) as r1:
+        print(r1.send_show_command('sh ip int br'))
+
+    del r1
